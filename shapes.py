70da049fa5424f007c1fa3c9812e872ce75ff7e4
e-farnsworth <farnsworth3e@gmail.com>
2024-05-08 12:02:35 -0600

    practicing merge conflicts

--- shapes.py
+++ shapes.py
@@ -1,6 +1,11 @@
 # This will help you calculate the shapes areas.
 
 def main():
-    print('This is our shapes program')
+    tri_area = calc_tri_area(4,7)
+    print(f'the area of the triangle is {tri_area}')
+
+def calc_tri_area(base, height):
+    area = base * height / 2
+    return area
 
 main()
