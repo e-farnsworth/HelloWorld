89f759912452bfcccbdc78198380be5e1595bf5c
e-farnsworth <farnsworth3e@gmail.com>
2024-05-08 11:56:23 -0600

    triangle area

--- shapes.py
+++ shapes.py
@@ -1,6 +1,11 @@
 # This will help you calculate the shapes areas.
 
 def main():
-    print('This is our shapes program')
+    triangle_area = calculate_triangle(4,7)
+    print(f'the area of the triangle is {triangle_area}')
+
+def calculate_triangle(base, height):
+    area = base * height / 2
+    return area
 
 main()
